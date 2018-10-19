e9f32dbdbf2de49ab9389ad6ab35bae536f6c29c
Abhinav Nigam <abnig19@gmail.com>
2018-10-18 23:25:02 -0400

    added java_main.yml

--- src/main/java/eureka/client/EurekaClientApplication.java
+++ src/main/java/eureka/client/EurekaClientApplication.java
@@ -16,20 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class EurekaClientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
 }
 
 @RestController
 class ServiceInstanceRestController {
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
-    }
+	@RequestMapping("/service-instances/{applicationName}")
+	public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+		return this.discoveryClient.getInstances(applicationName);
+	}
 }
